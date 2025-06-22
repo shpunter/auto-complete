dda1d282efb6189fe94671adcfc60861ef17cc72
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2025-06-22 12:09:15 +0200

    deploy error

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-import { data } from "./insert/data.ts";
-import { createKeyValuePair } from "./insert/insert.ts";
+import { Hono } from "jsr:@hono/hono";
+import { cors } from "jsr:@hono/hono/cors";
 
-data.map(createKeyValuePair);
+const app = new Hono();
+
+app.use(
+  "/api/*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Type", "Authorization"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+app.get("/", (c) => {
+  return c.json({
+    msg: "hello",
+  });
+});
+
+Deno.serve({ port: 8000 }, app.fetch);
