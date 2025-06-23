e66c0249c201c7b74a4a1d4a9d3ea0cfca995095
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2025-06-23 22:09:42 +0200

    add gen and insert loop  api

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,9 @@ app.get("/api/search/:search", async (c) => {
 app.get("/api/generate-and-insert/:number/:type", async (c) => {
   const { number, type } = c.req.param();
 
-  if (type !== "pair" && type !== "sentences") throw new HTTPException(404, { message: "type is not supported" })
+  if (type !== "pair" && type !== "sentences") {
+    throw new HTTPException(404, { message: "type is not supported" });
+  }
 
   const data = await generate(+number, type);
   const array = JSON.parse(data.text ?? "[]") as string[];
@@ -48,10 +50,30 @@ app.get("/api/generate-and-insert/:number/:type", async (c) => {
 
     kv.set(key, value);
   }
-  
+
   return c.json({
-    data: data.text
+    data: data.text,
   });
+});
+
+app.get("/api/generate-and-insert-loop/:number/", async (c) => {
+  const { number } = c.req.param();
+  let count = +number;
+
+  while (count > 0) {
+    const data = await generate(1000, "pair");
+    const array = JSON.parse(data.text ?? "[]") as string[];
+
+    for (let i = 0; i < array.length; i++) {
+      const { key, value } = createKeyValuePair(array[i]);
+
+      kv.set(key, value);
+    }
+
+    count -= 1;
+  }
+
+  return c.json("done");
 });
 
 Deno.serve({ port: 8000 }, app.fetch);
